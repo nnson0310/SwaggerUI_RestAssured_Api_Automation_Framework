de752751e4abf48a17281f709b79861d90d3a40d
Nguyễn Ngọc Sơn <nnson0310@gmail.com>
2022-10-22 17:19:20 +0700

    Implement authorize and generate token for user

---
+++ src/main/java/responses/Token.java
@@ -0,0 +1,12 @@
+package responses;
+
+public class Token {
+
+     public String token;
+
+     public String expires;
+
+     public String status;
+
+     public String result;
+}
---
+++ src/test/java/PreConditions.java
@@ -0,0 +1,29 @@
+import com.google.gson.Gson;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import responses.User;
+
+public class PreConditions {
+
+    public static void CreateNewAccount(String username, String password) {
+        RestAssured.baseURI = "https://bookstore.toolsqa.com";
+        RequestSpecification request = RestAssured.given();
+
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("userName", username);
+        requestParams.put("password", password);
+
+        request.header("Content-Type", "application/json");
+        request.body(requestParams.toJSONString());
+        Response response = request.post("/Account/v1/User");
+        User user = new Gson().fromJson(response.getBody().asString(), User.class);
+
+        Assert.assertEquals(response.getStatusCode(), 201);
+        Assert.assertNotNull(user.userID);
+        Assert.assertEquals(user.username, username);
+        Assert.assertEquals(user.books.length, 0);
+    }
+}
